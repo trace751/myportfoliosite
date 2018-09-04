bc0c3409380dfe75f0412153275ada2cde2f7bc0
Ram Dettmer <ramdettmer751@gmail.com>
2018-09-03 22:17:18 -0700

    added main text after intro text, customized CSS and added jquery elements for animation

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
 
 //Animate letter spacing name
-$('#hidden-content p').hide().fadeIn(800).animate({'letter-spacing': '15px'}, 2000);
+$('#hidden-content h1').hide().fadeIn(800).animate({'letter-spacing': '15px'}, 2000);
 //Fade in the subtitle
 $('#hidden-content span').hide().delay(2200).fadeIn(800);
 //Fade out the intro page
 $('#hidden-body').delay(4000).fadeOut(500);
-
 });
