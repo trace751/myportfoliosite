9e4e2618848377d322b78d3c713d24bdb1cc9b75
Ram Dettmer <ramdettmer751@gmail.com>
2018-09-11 00:12:07 -0700

    added page animation transitions

--- assets/script.js
+++ assets/script.js
@@ -6,4 +6,5 @@ $('#hidden-content h1').hide().fadeIn(800).animate({'letter-spacing': '15px'}, 2
 $('#hidden-content span').hide().delay(2200).fadeIn(800);
 //Fade out the intro page
 $('#hidden-body').delay(4000).fadeOut(500);
+
 });
--- assets/scrollreveal.js
+++ assets/scrollreveal.js
@@ -15,5 +15,26 @@ window.sr = ScrollReveal();
 			distance: '100px',
 			delay: 5000
 			});
-
 	});
+
+//Triggers the about me page
+let about = document.querySelector('.aboutme');
+about.addEventListener('click', function(e){
+	e.preventDefault();
+			sr.reveal('#about', {
+	duration: 2000,
+	origin: 'left',
+	distance: '100px',
+	});
+});
+
+//Triggers the contact page
+let contact = document.querySelector('.contact');
+contact.addEventListener('click', function(e){
+	e.preventDefault();
+			sr.reveal('#contact', {
+	duration: 2000,
+	origin: 'top',
+	distance: '100px',
+	});
+});
