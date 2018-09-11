545921e3e39b1c826c6c0bdd1c0ef5a8e1c3b45f
Ram Dettmer <ramdettmer751@gmail.com>
2018-09-11 10:29:19 -0700

    completed page animations for individual pages

---
+++ assets/scripts/pages.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+window.sr = ScrollReveal();
+		sr.reveal('.main-about', {
+			duration: 2000,
+			origin: 'left',
+			distance: '200px',
+			});
+
+		sr.reveal('.main-project', {
+			duration: 2000,
+			origin: 'left',
+			distance: '200px',
+			});
+
+
+		sr.reveal('.main-contact', {
+			duration: 2000,
+			origin: 'left',
+			distance: '200px',
+			});
+
+});
